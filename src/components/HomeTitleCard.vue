68c77ba2683675e78a52cc6fb9d2f925822590c2
Shumieb <shumiebc@gmail.com>
2025-09-13 01:47:54 +0100

    added hometitlecard

--- src/components/HomeTitleCard.vue
+++ src/components/HomeTitleCard.vue
@@ -2,8 +2,33 @@
 
 </script>
 
-<template>
-    <p>Home Title</p>
+<template>    
+    <section class="grid grid-cols-2 gap-2 mt-8 mb-8 mx-auto w-[80%] content-center">
+        <section class="py-2 ps-4 pe-6">
+            <p class="text-2xl">Hi, I am Shumie</p>
+            <p class="text-2xl">Software Developer</p>
+            <p class="text-lg mt-3 mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quia, exercitationem, impedit ut unde quam blanditiis 
+                perferendis vitae nihil mollitia minima id laudantium quaerat itaque 
+                non amet odit ipsum vero illum provident pariatur! Similique 
+                recusandae id, cumque quam est iusto aut explicabo officiis 
+                adipisci neque quo quae possimus quis praesentium tenetur!
+            </p>
+            <a 
+                href="/resume" 
+                role="button" 
+                class="bg-pink-900 text-white text-lg px-6 py-2 shadow rounded"
+            >View Resume</a>
+        </section>
+        <section>
+            <img 
+                src="https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg" 
+                alt="home picture"
+                class="w-80 h-80 rounded-full mx-auto"
+            >
+        </section>
+    </section>    
 </template>
 
 <style scoped>
